82e3a42c69d1388b5dbdbd6b9d9924767e85291a
Qayleef <muhammadqayleef@hotmail.com>
2024-08-22 12:54:24 +0800

    UI - New Navigation
    Home is the first and Profile is now last

--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fyp_umakan/features/authentication/screens/homepage/homepage.dart';
 import 'package:fyp_umakan/features/community/screens/community_main_page.dart';
+import 'package:fyp_umakan/features/discover/screens/discover.dart';
 import 'package:fyp_umakan/features/student_management/screens/student_profile.dart';
 import 'package:fyp_umakan/utils/constants/colors.dart';
 import 'package:fyp_umakan/utils/helpers/helper_functions.dart';
@@ -19,28 +20,42 @@ class NavigationMenu extends StatelessWidget {
     return Scaffold(
       bottomNavigationBar: Obx(
         () => NavigationBar(
-            height: 80,
-            elevation: 0,
-            selectedIndex: controller.selectedIndex.value,
-            onDestinationSelected: (index) => controller.selectedIndex.value = index,
-            backgroundColor: darkMode ? Colors.teal : TColors.teal,
-            indicatorColor: darkMode ? Colors.white.withOpacity(0.1) : Colors.white.withOpacity(0.1),
-
-            destinations: const [
-              NavigationDestination(icon: Icon(Iconsax.user, color: TColors.cream), label: 'Profile'),
-              NavigationDestination(icon: Icon(Iconsax.home, color: TColors.cream), label: 'Home'),
-              NavigationDestination(icon: Icon(Iconsax.emoji_happy, color: TColors.cream), label: 'Discover'),
-              NavigationDestination(icon: Icon(Iconsax.people, color: TColors.cream), label: 'Community'),
+          height: 80,
+          elevation: 0,
+          selectedIndex: controller.selectedIndex.value,
+          onDestinationSelected: (index) =>
+              controller.selectedIndex.value = index,
+          backgroundColor: darkMode ? Colors.teal : TColors.teal,
+          indicatorColor: darkMode
+              ? Colors.white.withOpacity(0.1)
+              : Colors.white.withOpacity(0.1),
+          destinations: const [
+            NavigationDestination(
+                icon: Icon(Iconsax.home, color: TColors.cream), label: 'Home'),
+            NavigationDestination(
+                icon: Icon(Iconsax.emoji_happy, color: TColors.cream),
+                label: 'Discover'),
+            NavigationDestination(
+                icon: Icon(Iconsax.people, color: TColors.cream),
+                label: 'Community'),
+            NavigationDestination(
+                icon: Icon(Iconsax.user, color: TColors.cream),
+                label: 'Profile'),
           ],
         ),
       ),
       body: Obx(() => controller.screens[controller.selectedIndex.value]),
     );
   }
-} 
+}
 
-class NavigationController extends GetxController{
+class NavigationController extends GetxController {
   final Rx<int> selectedIndex = 0.obs;
 
-  final screens = [const StudentProfilePageScreen(), const HomePageScreen(), Container(color: TColors.mustard,), const CommunityMainPageScreen()];
+  final screens = [
+    const HomePageScreen(),
+    const DiscoverPageScreen(),
+    const CommunityMainPageScreen(),
+    const StudentProfilePageScreen()
+  ];
 }
